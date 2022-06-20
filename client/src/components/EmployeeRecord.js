5c4a38672ec95887f6147b8012baf8da1ecb09eb
Philip Harmon <55819132+pharmon9847@users.noreply.github.com>
2022-06-20 13:27:48 -0400

    added some styling to EmployeeRecord.css

--- client/src/components/EmployeeRecord.js
+++ client/src/components/EmployeeRecord.js
@@ -5,13 +5,17 @@ function EmployeeRecord(props) {
     <div className="employee-record">
       <div className="employee-record__description">
         <div className="employee-record__name">{props.name}</div>
-        <div className="employee-record__street">{props.street}</div>
-        <div className="employee-record__city">{props.city}</div>
-        <div className="employee-record__state">{props.state}</div>
-        <div className="employee-record__zip">{props.zip}</div>
-        <div className="employee-record__DOB_month">{props.DOB_month}</div>
-        <div className="employee-record__DOB_day">{props.DOB_day}</div>
-        <div className="employee-record__year">{props.DOB_year}</div>
+        <div className="employee-record__address">
+          <div className="employee-record__address_street">{props.street}</div>
+          <div className="employee-record__address_city">{props.city}</div>
+          <div className="employee-record__address_state">{props.state}</div>
+          <div className="employee-record__address_zip">{props.zip}</div>
+        </div>
+        <div className="employee-birthday">
+          <div className="employee-birthday__DOB_month">{props.DOB_month}</div>
+          <div className="employee-birthday__DOB_day">{props.DOB_day}</div>
+          <div className="employee-birthday__DOB_year">{props.DOB_year}</div>
+        </div>
         <div className="employee-record__position">{props.position}</div>
       </div>
     </div>
