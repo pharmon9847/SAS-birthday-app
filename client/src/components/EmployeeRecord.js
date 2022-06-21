7f78ba6fb63db77b4c767a3d408ff68db11174b0
Philip Harmon <55819132+pharmon9847@users.noreply.github.com>
2022-06-20 20:16:22 -0400

    dynamically rendering lists

--- client/src/components/EmployeeRecord.js
+++ client/src/components/EmployeeRecord.js
@@ -7,9 +7,9 @@ const EmployeeRecord = (props) => {
   const [city, setCity] = useState(props.city);
   const [state, setState] = useState(props.state);
   const [zip, setZip] = useState(props.zip);
-  const [DOB_month, setDOB_month] = useState(props.DOB_month);
-  const [DOB_day, setDOB_day] = useState(props.DOB_day);
-  const [DOB_year, setDOB_year] = useState(props.DOB_year);
+  const [month, setMonth] = useState(props.month);
+  const [day, setDay] = useState(props.day);
+  const [year, setYear] = useState(props.year);
   const [position, setPosition] = useState(props.position);
   const clickHandler = () => {
     setName("updated!");
@@ -26,9 +26,9 @@ const EmployeeRecord = (props) => {
           <div className="employee-record__address_zip">{props.zip}</div>
         </div>
         <div className="employee-birthday">
-          <div className="employee-birthday__DOB_month">{props.DOB_month}</div>
-          <div className="employee-birthday__DOB_day">{props.DOB_day}</div>
-          <div className="employee-birthday__DOB_year">{props.DOB_year}</div>
+          <div className="employee-birthday__month">{props.month}</div>
+          <div className="employee-birthday__day">{props.day}</div>
+          <div className="employee-birthday__year">{props.year}</div>
         </div>
         <div className="employee-record__position">{props.position}</div>
       </div>
